2d4e42506b92ed3962838c1cc1e1eafec3140390
Pradeep Kumar <pradeep.k@shieldsquare.com>
2017-09-13 19:16:41 +0530

    - using two handlers one for image one for text

--- ngx_http_fun_module.c
+++ ngx_http_fun_module.c
@@ -4,21 +4,34 @@
 #include <cairo.h>
 #define M_PI 3.14159265
 /*Declare a handler*/
+
+//static u_char ngx_fun_string[];// = "This is fun!";
 static char *ngx_http_fun(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static char *ngx_http_fun2(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 /* The HTTP BOdY   The hello world string. */
 //static u_char ngx_fun_string[] = "This is Fun hello world";
 /*handler for the request */
 static ngx_int_t ngx_http_fun_handler(ngx_http_request_t *r);
-
+static ngx_int_t ngx_http_fun_handler2(ngx_http_request_t *r);
 
 static ngx_command_t ngx_http_fun_commands[] = {
 
-    { //entry point
+    { //entry point fun
       ngx_string("fun_http"), /* directive */
       NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS, /* location context and takes
                                             no arguments*/
       //A pointer to our handler-function
       ngx_http_fun, /* configuration setup function */
+      0, /* No offset. Only one context is supported. */
+      0, /* No offset when storing the module configuration on struct. */
+      NULL},
+
+    { //entry point for fun2
+      ngx_string("fun_http2"), /* directive */
+      NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS, /* location context and takes
+                                            no arguments*/
+      //A pointer to our handler-function
+      ngx_http_fun2, /* configuration setup function */
       0, /* No offset. Only one context is supported. */
       0, /* No offset when storing the module configuration on struct. */
       NULL},
@@ -199,6 +212,68 @@ static ngx_int_t ngx_http_fun_handler(ngx_http_request_t *r){
 
 
 
+//**trying to put something in the text**////
+
+
+static ngx_int_t ngx_http_fun_handler2(ngx_http_request_t * r){
+u_char ngx_fun_string[]="i am fine";  
+ngx_int_t rc;
+  ngx_buf_t * b;
+  ngx_chain_t out;
+  // we response to 'GET' and 'HEAD' requests only
+  if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
+  return NGX_HTTP_NOT_ALLOWED;
+     }
+  // discard request body, since we don't need it here
+  rc = ngx_http_discard_request_body(r);
+  if (rc != NGX_OK) {
+      return rc;
+  }
+ // set the 'Content-type' header
+  r->headers_out.content_type_len = sizeof("text/html") - 1;
+  r->headers_out.content_type.len = sizeof("text/html") - 1;
+  r->headers_out.content_type.data = (u_char * ) "text/html";
+
+// send the header only, if the request type is http 'HEAD'
+  if (r->method == NGX_HTTP_HEAD) {
+  r->headers_out.status = NGX_HTTP_OK;
+  r->headers_out.content_length_n = sizeof(ngx_fun_string) - 1;
+  return ngx_http_send_header(r);
+  }
+  // allocate a buffer for your response body
+  b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+  if (b == NULL) {
+  return NGX_HTTP_INTERNAL_SERVER_ERROR;
+  }
+// attach this buffer to the buffer chain
+  out.buf = b;
+  out.next = NULL;
+
+// adjust the pointers of the buffer
+  b->pos = ngx_fun_string;
+  b->last = ngx_fun_string + sizeof(ngx_fun_string) - 1;
+  b->memory = 1;
+// This buffer is in read-only memory
+// This means that filters should copy it, and not try to rewrite in place.
+  b->last_buf = 1; // this is the last buffer in the buffer chain
+
+// set the status line
+  r->headers_out.status = NGX_HTTP_OK;
+  r->headers_out.content_length_n = sizeof(ngx_fun_string) - 1;
+
+// send the headers of your response
+  rc = ngx_http_send_header(r);
+  if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+  return rc;
+  }
+// send the buffer chain of your response
+  return ngx_http_output_filter(r, &out);
+  }
+
+
+
+
+
 /*Attach the handler 
 in this handler-function we can validate on the incoming ngx_command_t and attach different handlers*/
 
@@ -207,6 +282,14 @@ static char *ngx_http_fun(ngx_conf_t * cf, ngx_command_t *cmd, void * conf){
 	clcf=ngx_http_conf_get_module_loc_conf(cf,ngx_http_core_module);
 	clcf->handler=ngx_http_fun_handler;//handler to process the fun directice
 	return NGX_CONF_OK;
+
+}
+
+static char *ngx_http_fun2(ngx_conf_t * cf, ngx_command_t *cmd, void * conf){
+        ngx_http_core_loc_conf_t * clcf;
+        clcf=ngx_http_conf_get_module_loc_conf(cf,ngx_http_core_module);
+        clcf->handler=ngx_http_fun_handler2;//handler to process the fun directice
+        return NGX_CONF_OK;
 
 }
 
